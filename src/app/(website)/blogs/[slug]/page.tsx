6969d7ea9f2cdf8ff31919da3b655ebb1efd6bef
badhon252 <khalidhossainbadhon@gmail.com>
2025-03-18 21:15:38 +0600

    refactoring code changes

--- src/app/(website)/blogs/[slug]/page.tsx
+++ src/app/(website)/blogs/[slug]/page.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import RecentBlogs from "./_components/recent-blogs";
 import BlogDetails from "./_components/blog-details";
+import NewsletterSection from "../../about/_components/_components/NewsletterSection";
 
 const page = () => {
   return (
     <div>
       <BlogDetails />
       <RecentBlogs />
+      <NewsletterSection />
     </div>
   );
 };
--- src/app/(website)/page.tsx
+++ src/app/(website)/page.tsx
@@ -3,6 +3,7 @@ import Hero from "./_components/hero";
 import { ServicesGrid } from "./_components/services-grid";
 import BlogsSection from "./_components/blogs-section";
 import ContactSection from "./_components/contact-section";
+import NewsletterSection from "./about/_components/_components/NewsletterSection";
 
 const page = () => {
   return (
@@ -11,6 +12,7 @@ const page = () => {
       <ServicesGrid />
       <BlogsSection />
       <ContactSection />
+      <NewsletterSection />
     </>
   );
 };
